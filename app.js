d924202bf6f34b81dee86a32b46e8b1f12a1382e
Stacey Brinkley <brinkleystacey12@gmail.com>
2022-04-12 15:11:13 -0400

    displaying posts

--- app.js
+++ app.js
@@ -1,22 +1,42 @@
 const url = 'https://jsonplaceholder.typicode.com/users';
+const data = document.getElementById('data');
+const posts = document.getElementById('posts')
 
 fetch(url)
-  .then((response) => response.json())
-  .then((json) => json.forEach(tableElements)
-  )
+    .then((response) => response.json())
+    .then((json) => json.forEach(tableElements)
+    )
 
-  function tableElements (element, index, arr){
-    arr[index] = document.getElementById('data').innerHTML +=
-    `<tr>
-        <td>${element.id}</td>
+function tableElements(element, index, arr) {
+    const tr = document.createElement('tr');
+    arr[index] = tr.innerHTML +=
+        `<td>${element.id}</td>
         <td>${element.name}</td>
-        <td>${element.username}</td>
-    </tr>`
-  }
+        <td>${element.username}</td>`;
+    tr.setAttribute('id', element.id);
+    data.appendChild(tr);
+    tr.addEventListener('click', getPosts);
 
-   
+}
 
-    
+
+function getPosts(event) {
+    fetch(`https://jsonplaceholder.typicode.com/posts?userId=${this.id}`)
+        .then((response) => response.json())
+        .then((json) => json.forEach(postElements)
+        )
+}
+
+function postElements(element, index, arr) {
+    arr[index] = posts.innerHTML +=
+        `<h5>Post #: ${element.id}</h5>
+        <p> <strong>Title:</strong> ${element.title}</p>
+        <p>${element.body}</p>`;
+}
+
+
+
+
 
 
 
